d88cfdb77e0bbb8873ffca103c07bc4ddca5c950
unknown <aleksandar.z.dukic@gmail.com>
2022-12-27 22:15:47 +0100

    First commit, example dotnet6 repo

---
+++ PlatformService/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt) 
+        {
+
+        }
+
+        protected override void OnConfiguring
+       (DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(databaseName: "InMem");
+        }
+
+        public DbSet<Platform> Platforms {get; set;}
+    }
+}
+
